3173dbc61967d451d4b669501e5eb9202fe64500
Vova228337 <vovavolt2552@gmail.com>
2021-06-23 12:34:02 +0300

    GameByDate not ready, GameById mabe

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const app = express();
 const db = require("./queries");
-const cors = require('cors');
-const port = process.env.PORT || 3300
+const cors = require("cors");
+const port = process.env.PORT || 3300;
 
 app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("Test send");
-// });
 
-app.get('/', (req, res) => {
- res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/api/GameByScore', db.getGameByScore);
-
+app.get("/api/GameByScore", db.getGameByScore);
+app.get("/api/GameByDate", db.getGameByDate);
+app.get("/api/GameById/:id", db.getGameById);
 
 // Слушаем сервер
 app.listen(port);
--- queries.js
+++ queries.js
@@ -27,6 +27,36 @@ const getGameByScore = (request, response) => {
   );
 };
 
+// Запрос на 10 свежих игр
+const getGameByDate = (request, response) => {
+  pool.query(
+    "SELECT * FROM game WHERE steamscore_percent is not null ORDER BY steamscore_percent DESC limit 10",
+    (error, results) => {
+      console.log(results);
+      console.log(error);
+      if (error) {
+        response.status(500).json({ success: false });
+      } else {
+        response.status(200).json(results.rows);
+      }
+    }
+  );
+};
+
+// Запрос на игру по id
+const getGameById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM game WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 module.exports = {
   getGameByScore,
+  getGameByDate,
+  getGameById,
 };
